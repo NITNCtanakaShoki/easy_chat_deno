02a15ddf8f2932e8093f04f46cd4b270d145929f
tanaka <lemo.nade.engineer.room@gmail.com>
2022-08-14 23:43:17 +0900

    github actions で CD

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.138.0/http/server.ts"
 
 const route = async (req: Request): Response => {
-    return new Response('Hello, World!')
+    return new Response('Hello, CD!')
 }
 
 await serve(route)
