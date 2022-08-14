0b957197420b97af4c50aabac7e7950b012db2a1
tanaka <lemo.nade.engineer.room@gmail.com>
2022-08-15 00:20:11 +0900

    serveDirの使い方を間違えていた気がする

--- server.ts
+++ server.ts
@@ -3,8 +3,11 @@ import { serveDir } from "https://deno.land/std@0.138.0/http/file_server.ts"
 
 const route = (req: Request): Promise<Response> => {
     return serveDir(req, {
-        fsRoot: "./public/"
-    })
+        fsRoot: "./public/",
+        urlRoot: "",
+        showDirListing: true,
+        enableCors: true,
+    });
 }
 
 await serve(route)
